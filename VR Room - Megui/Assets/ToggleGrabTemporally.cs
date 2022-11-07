c747d1b136c34c53538646dbbb596c3a7d75b955
Meguipascual <meguipascual@gmail.com>
2022-11-07 18:14:46 +0100

    All issues fixed by now. Light bulb added to flash light.

--- VR Room - Megui/Assets/ToggleGrabTemporally.cs
+++ VR Room - Megui/Assets/ToggleGrabTemporally.cs
@@ -5,6 +5,8 @@ public class ToggleGrabTemporally : MonoBehaviour
 {
     private XRRayInteractor _xrRayInteractor;
     private IXRSelectInteractable _xrGrabLayer;
+    private int _notGrabbable = 6;
+    private int _grabbable = 7;
 
     // Start is called before the first frame update
     void Start()
@@ -14,15 +16,44 @@ public class ToggleGrabTemporally : MonoBehaviour
 
     public void ChangeToNotGrabbable()
     {
-        if (_xrRayInteractor.interactablesSelected.Count == 1)
+        _xrGrabLayer = _xrRayInteractor.interactablesSelected[0];
+        if (_xrGrabLayer.transform.gameObject.layer == _grabbable)
         {
-            _xrGrabLayer = _xrRayInteractor.interactablesSelected[0];
-            _xrGrabLayer.transform.gameObject.layer = 6;
+            _xrGrabLayer.transform.gameObject.layer = _notGrabbable;
+            var componentsInChildren = _xrGrabLayer.transform.gameObject.GetComponentsInChildren<Transform>(includeInactive : true);
+            Debug.Log(componentsInChildren.Length);
+            if (componentsInChildren.Length == 1)
+            {
+                return;
+            }
+            else
+            {
+                foreach (Transform child in componentsInChildren)
+                {
+                    child.gameObject.layer = _notGrabbable;
+                }
+            }
         }
     }
 
     public void ChangeToDefault()
     {
-        _xrGrabLayer.transform.gameObject.layer = 0;
+        if (_xrGrabLayer.transform.gameObject.layer == _notGrabbable)
+        {
+            _xrGrabLayer.transform.gameObject.layer = _grabbable;
+            var componentsInChildren = _xrGrabLayer.transform.gameObject.GetComponentsInChildren<Transform>(includeInactive: true);
+            Debug.Log(componentsInChildren.Length);
+            if (componentsInChildren.Length == 1)
+            {
+                return;
+            }
+            else
+            {
+                foreach (Transform child in componentsInChildren)
+                {
+                    child.gameObject.layer = _grabbable;
+                }
+            }
+        }
     }
 }
